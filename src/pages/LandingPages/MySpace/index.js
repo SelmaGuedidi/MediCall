4a761fa631a5c2976e0abd2774b11ab66569ff9a
ad <karim.ellouze@insat.ucar.tn>
2023-05-14 13:13:43 +0100

    notification2

--- src/pages/LandingPages/MySpace/index.js
+++ src/pages/LandingPages/MySpace/index.js
@@ -1,65 +1,76 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Snackbar, SnackbarContent, IconButton, Avatar } from "@material-ui/core";
-import { Close } from "@material-ui/icons";
+import { useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Grid,
+  Paper,
+  Divider,
+  makeStyles,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  snackbar: {
-    margin: theme.spacing(1),
-  },
-  content: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white,
-    display: "flex",
-    alignItems: "center",
-  },
-  message: {
-    marginLeft: theme.spacing(2),
-    flexGrow: 1,
-  },
-  avatar: {
-    backgroundColor: theme.palette.secondary.main,
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500,
   },
 }));
 
+const appointments = [
+  { id: 1, date: "2023-05-12", time: "10:00 AM", location: "New York" },
+  { id: 2, date: "2023-05-13", time: "2:00 PM", location: "Los Angeles" },
+  { id: 3, date: "2023-05-14", time: "4:00 PM", location: "Chicago" },
+];
+
 function NotificationCenter() {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
+  const [selectedAppointment, setSelectedAppointment] = useState(null);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
+  const handleAppointmentClick = (appointment) => {
+    setSelectedAppointment(appointment);
   };
 
   return (
-    <div>
-      <IconButton color="inherit" onClick={handleOpen}>
-        <Avatar className={classes.avatar}>N</Avatar>
-      </IconButton>
-      <Snackbar
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        className={classes.snackbar}
-      >
-        <SnackbarContent
-          className={classes.content}
-          message={<span className={classes.message}>This is a notification message!</span>}
-          action={[
-            <IconButton key="close" color="inherit" onClick={handleClose}>
-              <Close />
-            </IconButton>,
-          ]}
-        />
-      </Snackbar>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+          <Typography variant="h6">Appointments</Typography>
+          <Divider />
+          <List>
+            {appointments.map((appointment) => (
+              <ListItem
+                key={appointment.id}
+                button
+                selected={selectedAppointment?.id === appointment.id}
+                onClick={() => handleAppointmentClick(appointment)}
+              >
+                <ListItemText
+                  primary={appointment.date}
+                  secondary={`${appointment.time} @ ${appointment.location}`}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Grid>
+      <Grid item xs={8}>
+        {selectedAppointment ? (
+          <Paper className={classes.paper}>
+            <Typography variant="h6">Appointment Details for {selectedAppointment.date}</Typography>
+            <Divider />
+            <Typography variant="subtitle1">Time: {selectedAppointment.time}</Typography>
+            <Typography variant="subtitle1">Location: {selectedAppointment.location}</Typography>
+          </Paper>
+        ) : (
+          <Paper className={classes.paper}>
+            <Typography variant="h6">No appointment selected</Typography>
+          </Paper>
+        )}
+      </Grid>
+    </Grid>
   );
 }
+
 export default NotificationCenter;
